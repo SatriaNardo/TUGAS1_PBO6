fefdab7d42582b83487ea84e2025fb5b075d6693
Cruz <114635273+SatriaNardo@users.noreply.github.com>
2023-04-16 23:35:45 +0700

    Add files via upload

---
+++ Game/src/game/Game.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package game;
+
+/**
+ *
+ * @author REPUBLIC OF GAMERS
+ */
+public class Game {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        charMaker maker = new charMaker();
+        maker.setVisible(true);
+        
+    }
+    
+}
---
+++ Game/src/game/charStat.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package game;
+
+/**
+ *
+ * @author REPUBLIC OF GAMERS
+ */
+public class charStat {
+    public int health;
+    public int defense;
+    public int attack;
+    public String name;
+}
